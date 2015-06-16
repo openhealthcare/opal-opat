bc957ae6f4412aafb4de7c11ab97bc04abf0af04
David Miller <david@deadpansincerity.com>
2015-06-16 13:08:15 +0100

    Update to Django 1.7

--- runtests.py
+++ runtests.py
@@ -14,15 +14,34 @@ settings.configure(DEBUG=True,
                    },
                    OPAL_OPTIONS_MODULE = 'opat.tests.dummy_options_module',
                    ROOT_URLCONF='opat.urls',
+                   COMPRESS_ROOT='/tmp/',
+                   STATIC_URL = '/assets/',
+                   MIDDLEWARE_CLASSES = (
+                       'django.middleware.common.CommonMiddleware',
+                       'django.contrib.sessions.middleware.SessionMiddleware',
+                       'opal.middleware.AngularCSRFRename',
+                       'django.middleware.csrf.CsrfViewMiddleware',
+                       'django.contrib.auth.middleware.AuthenticationMiddleware',
+                       'django.contrib.messages.middleware.MessageMiddleware',
+                       'opal.middleware.DjangoReversionWorkaround',
+                       'reversion.middleware.RevisionMiddleware',
+                       'axes.middleware.FailedLoginMiddleware',
+                   ),
                    INSTALLED_APPS=('django.contrib.auth',
                                    'django.contrib.contenttypes',
                                    'django.contrib.sessions',
                                    'django.contrib.admin',
+                                   'django.contrib.staticfiles',
+                                   'compressor',
                                    'opal',
-                                   'opat',))
+                                   'opat',),)
 
 from opat.tests import dummy_options_module
 from opat.tests import dummy_opal_application
+
+import django
+django.setup()
+
 
 from django.test.runner import DiscoverRunner
 test_runner = DiscoverRunner(verbosity=1)
