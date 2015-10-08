22a1acf1739f5b1224e852c8bfa3918e9768e97e
David Miller <david@deadpansincerity.com>
2015-10-08 17:16:05 +0100

    Capture primary infective diagnosis

--- opat/models.py
+++ opat/models.py
@@ -1,8 +1,6 @@
 """
 Models for opat
 """
-# 
-# Warning - even if you don't have any models, please don't delete this file.
-# Some parts of Django require you to have something it can import called 
-# opat.models in order for us to let you be a Django app.
-#
+from opal.core import lookuplists
+
+class OPATInfectiveDiagnosis(lookuplists.LookupList): pass
